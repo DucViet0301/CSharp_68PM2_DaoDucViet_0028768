ce6b4008a87d4bbf683335f2acfb4d829d951bf7
Duc Viet <vietdao464@gmail.com>
2026-05-15 08:35:41 +0700

    Add Demo Layout QLSV

--- BaiTapLonC#Winform/Program.cs
+++ BaiTapLonC#Winform/Program.cs
@@ -16,7 +16,7 @@ namespace BaiTapLonC_Winform
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new QLSinhVien());
         }
     }
 }
